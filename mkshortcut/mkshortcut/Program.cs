271ea177469d223edf185eee240ba9a281376a90
Akira Kawamata <autumn@piedey.co.jp>
2024-01-03 19:52:07 +0900

    fix it

--- mkshortcut/mkshortcut/Program.cs
+++ mkshortcut/mkshortcut/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Runtime.InteropServices;
+
+if( args.Length != 2 )
+{
+    Console.WriteLine("Usage: mkshortcut <shortcut_path> <target_path>");
+    return;
+}
+create(args[0], args[1]);
+
+void create(string lnkPath,string fullPath)
+{
+    dynamic? shell = null;   // IWshRuntimeLibrary.WshShell
+    dynamic? lnk = null;     // IWshRuntimeLibrary.IWshShortcut
+    try
+    {
+#pragma warning disable CA1416 // プラットフォームの互換性を検証
+        // available in Windows only
+        var type = Type.GetTypeFromProgID("WScript.Shell");
+#pragma warning restore CA1416 // プラットフォームの互換性を検証
+        if (type != null)
+        {
+            shell = Activator.CreateInstance(type);
+            if (shell != null)
+            {
+                lnk = shell.CreateShortcut(lnkPath);
+                if (lnk != null)
+                {
+                    lnk.TargetPath = fullPath;
+                    lnk.Save();
+                    Console.WriteLine($"created {lnkPath} to {fullPath}");
+                }
+            }
+        }
+    }
+    finally
+    {
+        if (lnk != null) Marshal.ReleaseComObject(lnk);
+        if (shell != null) Marshal.ReleaseComObject(shell);
+    }
+}
